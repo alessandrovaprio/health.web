a40bf24f1c83559e46895d5b2ab6bfeaf04a25c6
alessandrovaprio <alessandro.vaprio@gmail.com>
2018-07-12 08:11:02 +0200

    first commit

---
+++ Health.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Health.Web.Models;
+
+namespace Health.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        static readonly string[] Devices = new[]
+        {
+            "Fitbit Alta",
+            "Fitbit Charge 2",
+            "Fitbit Ionic",
+        };
+
+        public IActionResult Index()
+        {
+            var random = new Random();
+
+            var model =
+                from i in Enumerable.Range(0, 10)
+                let deviceId = Devices[random.Next(2)]
+                select new Heartbeat
+                {
+                    Id = i,
+                    DeviceId = deviceId,
+                    Timestamp = DateTime.Now,
+                    Value = random.Next(50, 80),
+                };
+
+            return View(model);
+        }
+    }
+}
---
+++ Health.Web/Models/Heartbeat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Health.Web.Models
+{
+    public class Heartbeat
+    {
+        public long Id { get; set; }
+
+        public string DeviceId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public int Value { get; set; }
+    }
+}
